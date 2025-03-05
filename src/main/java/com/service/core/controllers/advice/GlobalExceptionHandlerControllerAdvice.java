7a84a666f02aaaefd4dbe2b499eb046741eec401
ThreeAi <alexandrovaa2003@gmail.com>
2025-03-06 02:38:28 +0300

    add common exception handler

--- src/main/java/com/service/core/controllers/advice/GlobalExceptionHandlerControllerAdvice.java
+++ src/main/java/com/service/core/controllers/advice/GlobalExceptionHandlerControllerAdvice.java
@@ -1,17 +1,30 @@
 package com.service.core.controllers.advice;
 
+import com.service.core.models.dto.ExceptionResponse;
 import com.service.core.security.services.exception.EmptyTokenRefreshException;
+import io.swagger.v3.oas.annotations.Hidden;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@RestControllerAdvice
+@ControllerAdvice
+@Hidden
 public class GlobalExceptionHandlerControllerAdvice {
+
+    Logger logger = LogManager.getLogger(GlobalExceptionHandlerControllerAdvice.class);
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionResponse> handleAllExceptions(Exception ex) {
+        logger.error("Exception handler", ex.fillInStackTrace());
+        return new ResponseEntity<>(new ExceptionResponse(ex.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
     @ExceptionHandler(EmptyTokenRefreshException.class)
     public ResponseEntity<String> handleEmptyTokenExceptions(EmptyTokenRefreshException ex) {
---
+++ src/main/java/com/service/core/models/dto/ExceptionResponse.java
@@ -0,0 +1,4 @@
+package com.service.core.models.dto;
+
+public record ExceptionResponse(String message) {
+}
